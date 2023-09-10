a78642efa5aa19476891b92ae08f7d20633d3a4f
Suryanarayanan Balaji <112913550+Suryanarayanan-Balaji@users.noreply.github.com>
2023-09-10 14:18:01 -0400

    Added pretraining.py

---
+++ pretraining.py
@@ -0,0 +1,86 @@
+from transformers import LineByLineTextDataset, DataCollatorForLanguageModeling
+from transformers import BertTokenizer,  BertConfig, BertForMaskedLM
+from transformers import RobertaTokenizer, RobertaConfig, RobertaForMaskedLM
+from transformers import Trainer, TrainingArguments
+import torch
+import logging
+logging.basicConfig(level = logging.ERROR)
+import os
+import yaml
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+location = os.getcwd() # /home/suryabalaji/GPT_MolBERTa
+
+with open(os.path.join(location, 'config_finetune.yaml'), 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+if config['model'] == 'bert':
+    path = os.path.join(location, 'bert')
+    filepath = os.path.join(path, 'pretrain_data_bert.txt')
+
+    vocab_file_directory = os.path.join(path, 'vocab.txt')
+
+    tokenizer = BertTokenizer.from_pretrained(vocab_file_directory)
+
+    configuration = BertConfig(**config['model_bert'])
+    
+    model = BertForMaskedLM(configuration)
+
+    print(f'Number of parameters: {model.num_parameters()}')
+
+elif config['model'] == 'roberta':
+    path = os.path.join(location, 'roberta')
+    train_file = os.path.join(path, 'pretrain_data_roberta_train.txt')
+    eval_file = os.path.join(path, 'pretrain_data_roberta_eval.txt')
+
+    vocab_file_directory = os.path.join(path, 'tokenizer_folder')
+
+    tokenizer = RobertaTokenizer(
+        os.path.join(vocab_file_directory, 'vocab.json'),
+        os.path.join(vocab_file_directory, 'merges.txt')
+    )
+
+    configuration = RobertaConfig(**config['model_roberta'])
+    
+    model = RobertaForMaskedLM(configuration)
+
+    print(f'Number of parameters: {model.num_parameters()}')
+
+train_dataset = LineByLineTextDataset( 
+    tokenizer = tokenizer,
+    file_path = train_file,
+    block_size = 128
+)
+
+eval_dataset = LineByLineTextDataset( 
+    tokenizer = tokenizer,
+    file_path = eval_file,
+    block_size = 128
+)
+
+print(f'No. of lines: {len(train_dataset) + len(eval_dataset)}')
+
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer = tokenizer, mlm = True, mlm_probability = 0.15
+)
+
+training_args = TrainingArguments(
+    output_dir = path,
+    overwrite_output_dir = True,
+    num_train_epochs = 7,
+    per_device_train_batch_size = 30,
+    save_steps = 1000,
+    save_total_limit = 5
+)
+
+trainer = Trainer(
+    model = model,
+    args = training_args,
+    data_collator = data_collator, 
+    train_dataset = train_dataset,
+    compute_metrics = compute_metrics,
+)
+
+trainer.train()
+trainer.save_model(path)
